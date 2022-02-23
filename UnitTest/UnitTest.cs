f101916da15b10f41b2184dc0db9bc5c27df9fc3
sonokagi <sonokagi@gmail.com>
2022-02-23 16:40:53 +0900

    Htmlデータ検証用の関数を追加

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -85,10 +85,43 @@ namespace UnitTest
     }
     public static class Helper
     {
+        // Textデータが期待通りかチェックする
         public static void CheckText(string expect)
         {
             string actual = Clipboard.GetText(TextDataFormat.Text);
             Assert.AreEqual(actual, expect);
+        }
+
+
+        // Htmlデータの <!--StartFragment--> ～ <!--EndFragment--> の部分が期待通りかチェックする
+        public static void CheckHtmlFragmentPart(string expect)
+        {
+            string raw_data = Clipboard.GetText(TextDataFormat.Html);
+            string actual = extractFragmentPart(raw_data);
+            Assert.AreEqual(actual, expect);
+        }
+
+        static string extractFragmentPart(string html)
+        {
+            string start_keyword = "<!--StartFragment-->";  // 開始キーワード
+            string end_keyword = "<!--EndFragment-->";    // 終了キーワード
+            int start_index = html.IndexOf(start_keyword);  // 開始キーワードの検出位置
+            int end_index = html.IndexOf(end_keyword);      // 終了キーワードの検出位置
+
+            // 正常にキーワードが見つかった場合、各インデックスは0より大きくなるはず
+            if (start_index > 0 && end_index > 0)
+            {
+                int start = start_index + start_keyword.Length;
+                int length = end_index - start;
+                string part = html.Substring(start, length);
+                return part;
+            }
+            // 異常時はテストを失敗させる
+            else
+            {
+                Assert.Fail("cannot find Fragment keywords in data.");
+                return String.Empty;
+            }
         }
 
         public static void CheckHasTextData()
