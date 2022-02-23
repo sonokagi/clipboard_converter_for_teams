dc1a1fc66c78c67bb656c0484cac370946597e06
sonokagi <sonokagi@gmail.com>
2022-02-23 15:51:27 +0900

    ClipboardHelperの挙動確認のため、学習テスト追加

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Windows.Forms;
 using clipboard_converter_for_teams;
 
 namespace UnitTest
@@ -12,6 +13,38 @@ namespace UnitTest
         {
             ClipboardConverterCollection.Execute();
             Assert.Fail("first step");
+        }
+    }
+
+    [TestClass]
+    public class ClipboardHelperLearningTest
+    {
+        //分かったこと
+        //- htmlデータだけをクリップボードに設定することはできない
+        //  plainText引数に null を渡しても、空のテキストが設定されてしまう
+
+        [TestMethod]
+        public void SetNullToText_Then_ClipboardHasEmptyText()
+        {
+            // テキストを null で指定した場合
+            ClipboardHelper.CopyToClipboard("html", null);
+            // テキストデータを持つ
+            Assert.AreEqual(Clipboard.ContainsData(DataFormats.Text), true);
+            // テキストデータは空になる
+            string actual = Clipboard.GetText(TextDataFormat.Text);
+            Assert.AreEqual(actual, String.Empty);
+        }
+
+        [TestMethod]
+        public void SetEmptyToText_Then_ClipboardHasEmptyText()
+        {
+            // テキストを空で指定した場合
+            ClipboardHelper.CopyToClipboard("html", String.Empty);
+            // テキストデータを持つ
+            Assert.AreEqual(Clipboard.ContainsData(DataFormats.Text), true);
+            // テキストデータは空になる
+            string actual = Clipboard.GetText(TextDataFormat.Text);
+            Assert.AreEqual(actual, String.Empty);
         }
     }
 }
